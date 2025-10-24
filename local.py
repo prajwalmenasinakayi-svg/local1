8cfdbddb5bf46f5f4c78b62b403fa2c50e28d3ac
prajwalmenasinakayi-svg <prajwalmenasinakayi@gmail.com>
2025-10-24 08:52:16 +0530

    Update local.py

--- local.py
+++ local.py
@@ -1,7 +1,7 @@
 try:
-  a=float(input("enter first number:"))
-  b=float(input("enter second number :"))
-  print(f"Addition : {a+b}")
-  print(f"subtraction:{a-b}")
+    a = float(input("enter first number: "))
+    b = float(input("enter second number: "))
+    print(f"Addition : {a + b}")
+    print(f"Subtraction : {a - b}")
 except ValueError:
-  print ("please enter valid numbers.")
+    print("please enter valid numbers.")
